ff0b931e364be7c25d13fa0faaa7139fd7cfac33
muhammad umair <muhammadumairabbasi637@gmail.com>
2023-11-03 00:24:16 -0700

    class three completed,conversions of datatypes

---
+++ 03_conversionOperation.js
@@ -0,0 +1,103 @@
+// let score = 33;
+
+// console.log(typeof score) // same
+// console.log(typeof (score)) // same
+
+// convert in number to other datatypes
+
+// let score = '33'
+// console.log(typeof score) // string
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber) // number convert in into number
+
+
+// let score = '33abc'
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber) // NaN
+
+
+// let score = null
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)  // 0
+
+
+// let score = undefined
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)  // NaN
+
+
+
+// let score = true
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber) // 1
+
+
+
+
+// let score = false
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber) // 0
+
+
+
+
+// let score = 'umair'
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)  // NaN
+
+
+// Notes 
+// '33' => 33;
+// '33abc' => NaN;
+// console.log(typeof NaN) // number
+// true => 1; false => 0
+
+
+// _____X____X____
+
+// convert in boolean to other datatypes 
+
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn) // true
+
+
+// let isLoggedIn = 0
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn) // false
+
+
+// let isLoggedIn = ''
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn) // false
+
+
+// let isLoggedIn = 'umair'
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn) // true
+
+
+// 1 => true
+// 0 => false
+// '' => false
+// 'umair' => true
+
+// ______X______X_____
+
+
+// convet in string to other datatypes 
+
+// let someNumber = 33 ;
+// let stringNumber = String(someNumber)
+// console.log(stringNumber) // 33
+// console.log(typeof stringNumber) // string
+
+// ____X_______X______
