e9a11918f7d15b6411f5e0f01f9d1732b237977a
muhammad umair <muhammadumairabbasi637@gmail.com>
2023-11-02 22:56:11 -0700

    updated summary of first lecture

--- 01_variables.js
+++ 01_variables.js
@@ -30,7 +30,16 @@ console.log(accountCity)
 
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
 
-// Summary of this class 
+// Summary of this class
+
+// Lecture  # 1:
+// Variables and Data Types
+// const = can't be re-assigned a value and can't be declared again  (block scope)
+// let = can be re-assigned a value but can't be declared again (block scope)
+// var = it can be re-assigned a value and it can also be declared again  (Global scope)
+
+// => Never use var in your code instead of let because of block and functional scope issues.
+// # console.table([group of variables you want to show in tabular form])
 
 /*
 Prefer not to use var
