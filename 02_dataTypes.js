2882d6640a521006d05babe1ddf3b64fe97a3dfb
muhammad umair <muhammadumairabbasi637@gmail.com>
2023-11-02 23:39:04 -0700

    class two completed, concept of datatypes

---
+++ 02_dataTypes.js
@@ -0,0 +1,43 @@
+// "use strict";  // treat all Js code as newer version
+
+// DataTypes
+
+// NN,SS,BB,U 
+// Primitive DataTypes 
+
+// Number => 2 to power 53
+// Null
+// String => "".''
+// Symbol
+// Boolean => true/false
+// BigInt
+// Undefined => uniqueness
+
+
+// Non Primitives Data types
+
+// Object 
+
+// console.log(typeof "umair"); // string
+// console.log(typeof 123); // number
+// console.log(typeof null); // object
+// console.log(typeof undefined) // undefined
+
+
+// Summary 
+
+// number = 2 ^ 53
+// bigint = for large numbers
+// string = "assembly of characters"
+// boolean = true or false
+// null = stand alone value (type = object)
+// undefined =  place holder for a value which is not defined yet (type = undefined)
+// symbol = uniquness
+
+
+// # "use strict";   treat all code as newer version of JS
+
+
+
+
+ 
