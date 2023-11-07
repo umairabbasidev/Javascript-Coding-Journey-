f219f5bf52c59856bb59bd00a8c1174ce88334ca
muhammad umair <muhammadumairabbasi637@gmail.com>
2023-11-06 22:42:29 -0800

    class 7 completed, concept of number and Math

---
+++ 07_number_and_math.js
@@ -0,0 +1,39 @@
+// Topic = Number and maths
+
+const score = 56;
+// console.log(score)
+
+const num = new Number(100.466)
+// console.log(num)
+// console.log(num.toString().length)
+// console.log(num.toFixed(1))
+
+const otherNumber = 123.8966;
+// console.log(otherNumber.toPrecision(3))
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString())  // 1,000,000;
+
+// ___________X___________X_______
+
+// ********* Math *************
+
+// console.log(Math)
+
+// console.log(Math.abs(-4))  //  4negative converted in to positive
+// console.log(Math.round(4.3)) // 4
+// console.log(Math.round(4.5)) // 5
+// console.log(Math.ceil(4.000001)) // 5
+// console.log(Math.floor(4.9)) // 4
+// console.log(Math.min(4,8,2,6))
+// console.log(Math.max(95 ,4,8,2,6))
+
+
+// console.log(Math.random()) //0-1
+// console.log(Math.random() *10 + 1)
+
+// console.log(Math.floor(Math.random()*10) + 1)
+
+const min = 10;
+const max = 20;
+// console.log(Math.floor(Math.random() * (max-min + 1)) +min)
