4f931e87d052f8a4d20e1af22aea5dc1ef98fecc
muhammad umair <muhammadumairabbasi637@gmail.com>
2023-11-08 12:10:31 -0800

    class 8 completed, concept of Dates in js

---
+++ 08_dates-in-js.js
@@ -0,0 +1,37 @@
+// Topic
+
+// let myDate = new Date()
+// console.log(myDate) // Wed Nov 08 2023 11:24:16 GMT-0800 (Pacific Standard Time)
+
+// console.log(myDate.toString()) //  Wed Nov 08 2023 11:24:16 GMT-0800 (Pacific Standard Time)
+
+// console.log(myDate.toDateString()) // Wed Nov 08 2023
+
+// console.log(myDate.toISOString()) // 2023-11-08T19:28:47.888Z
+// console.log(myDate.toLocaleString()) // 08/11/2023, 11:30:05
+
+// console.log(myDate.toLocaleTimeString())  // 11:31:47
+
+// console.log(myDate.getDate())// 8
+// console.log(myDate.getDay()) // 3
+// console.log(myDate.getFullYear()) // 2023
+// console.log(myDate.getHours()) //11
+// console.log(myDate.getMinutes()) //34
+// console.log(myDate.setFullYear('2024'))
+// console.log(myDate.setDate('30'))
+
+
+// let myCreatedDate = new Date('2024,1,1')
+// let myCreatedDate = new Date(2024,0,1,12,5,15) // 01/01/2024, 12:05:15
+// console.log(myCreatedDate.toLocaleString())
+// // console.log(myCreatedDate)
+// console.log(myCreatedDate.toDateString())  // Tue Jan 02 2024
+
+// console.log(new Date(2023,10,8,11,53,10).toLocaleString())
+
+
+
+let myCreatedDate = new Date("2023-10-8")
+console.log(myCreatedDate.toLocaleString())
+
+ 
