ba0da74d70b6b970bbe2bc221632790f78ea16d9
muhammad umair <muhammadumairabbasi637@gmail.com>
2023-11-10 11:10:50 -0800

    class13,concpt of object de-structure and api

---
+++ 13_object-de-structing-and-json-api.js
@@ -0,0 +1,32 @@
+//  Topic : object-de-structing-and-json-api
+
+const courseInformation = {
+ coursename:"js in hindhi",
+ price:"999",
+ courseInstructor:"hitesh"
+}
+console.log(courseInformation)
+console.log(courseInformation.courseInstructor)
+
+// object de structing 
+const {coursename:name}= courseInformation
+
+const {price:rupe}= courseInformation
+
+// console.log(name)
+// console.log(rupe)
+
+
+// APi concept and json
+
+// {
+//  "name":"hitesh",
+//  "coursename":"js in hindi",
+//  "price":"free"
+
+// }
+
+
+
+
+
