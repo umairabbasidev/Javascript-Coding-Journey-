38136c8fb4858124774f6850bf8f528c673866ff
muhammad umair <muhammadumairabbasi637@gmail.com>
2023-11-18 12:50:40 +0500

    class 20 completd, concepet of while and do while Loop in dept with all data types in js

---
+++ 20_while-and-doWhile-loop-in-js.js
@@ -0,0 +1,137 @@
+// Topic While and do while
+
+// let index=0;
+// while (index<=10) {
+//  console.log(`Value of index is:${index}`)
+//  // index++
+//  index+=2
+// }
+
+// let arr = ['umair','uzair','asad','noman','hamid']
+// let i =0
+// while (i<arr.length) {
+//  console.log(arr[i])
+//  i+=2
+// }
+
+// let name = 'Umair';
+// let i=0;
+// while (i<name.length) {
+//  console.log(name[i])
+//  i++
+// }
+
+// let myArray = ['umair','uzair','asad','tayyab',1,2,3,4,5]
+// let i=0;
+// while (i<myArray.length) {
+//  console.log(`The value is ${myArray[i]}`)
+//  i++
+// }
+
+// let myObj = {
+//  name:"umair",
+//  age:21,
+//  skill:"Developer"
+// }
+
+// let i=0;
+// while (i<Object.keys(myObj).length) {
+//  console.log(`The user ${Object.keys(myObj)[i]} is : ${myObj[Object.keys(myObj)[i]]}`)
+//  // i++ same
+//  // i+=1 same
+//  i=i+1 same
+// }
+
+// let i = 0;
+// let j = 0;
+// while (i <= 10) {
+//   console.log(`Outer loop value is :${i}`);
+//   i++
+
+//   while (j <= 10) {
+//     console.log(`Inner loop value is :${j}`);
+//     j++
+//   }
+// }
+
+
+
+// let i = 0;
+// let j = 0;
+// while (i <= 10) {
+//   console.log(`Outer loop value is :${i}`);
+//   i++
+
+//   while (j <= 10) {
+//     console.log(`${i} x ${j} = ${i*j}`);
+//     j++
+//   }
+// }
+
+
+
+
+// Do while
+
+// let i=0
+// do {
+//  console.log(i)
+//  i++
+// } while (i<=10);
+
+
+// let i=20
+// do {
+//  console.log(i)  // 20
+//  i++
+// } while (i<=10);
+
+// let arr = ['umair','uzair','asad']
+// let i=0;
+// do {
+//  console.log(arr[i])
+//  i++
+// }while (i<arr.length) 
+
+
+// let name = 'umair';
+// let i=0;
+// do {
+//  console.log(name[i])
+//  i++
+ 
+// } while (i<name.length);
+
+
+
+// let myObj = {
+//  name:"umair",
+//  age:21,
+//  skill:"Developer"
+// }
+// let i=0
+// do {
+ // console.log(`The user ${Object.keys(myObj)[i]} is : ${myObj[Object.keys(myObj)[i]]}`)
+ // i++
+ // i+=1
+ // i=i+1
+// } while (i<Object.keys(myObj).length);
+
+
+let i=0;
+let j=0;
+
+// do {
+//  console.log(`Ouetr Loop Value is :${i}`)
+ 
+//  i++
+
+//  do {
+//   console.log(`${i} x ${j} = ${i*j}`)
+//   j++ 
+  
+//  } while (j<=10);
+
+
+ 
+// } while (i<=10);
