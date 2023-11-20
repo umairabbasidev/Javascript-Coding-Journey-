6e7ae48fe52d8b1a6646e49ca1296c7d5f93ea63
muhammad umair <muhammadumairabbasi637@gmail.com>
2023-11-20 10:51:23 +0500

    class 22 complted concept of event in dept

---
+++ 22_events-in-js.js
@@ -0,0 +1,14 @@
+// Topic Events
+
+// type, timestamp,defaultPrevented,target, toElement,srcElement,surrentTarget,
+// clentX,clientY,screenX,screenY,offSetX,offSetY
+// altkey,ctrlkey,shifkey,keycode
+
+// event propogation
+// two context hota h 
+// 1) event bubbling third parameter in and its default   addEventListener('click',function(e){ },false
+
+// 2) event capturing third parameter in   addEventListener('click',function(e){ },true
+
+
+// 1) event bubbling
